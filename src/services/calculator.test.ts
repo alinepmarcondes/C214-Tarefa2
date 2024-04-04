3fc9e8f4acc9f5c20e9f9f4f128a32b18d1a4a2c
alinepmarcondes <aline.patta@ges.inatel.br>
2024-04-03 21:44:28 -0300

    :recycle: test: add tests

---
+++ src/services/calculator.test.ts
@@ -0,0 +1,78 @@
+import { Calculator } from './Calculator';
+import { CalculatorRepository } from '../repository/CalculatorRepository';
+import { Count } from "../models/Calc";
+
+const makeRepositoryStub = (): CalculatorRepository => {
+  class CalculatorStub implements CalculatorRepository {
+    add (count : Count) {
+      return {
+        success: true,
+        error: null
+      }
+    }
+
+    subtract (count : Count) {
+      return {
+        success: this.result,
+        error: null
+      }
+    }
+
+    multiply (count : Count) {
+      return {
+        success: this.result,
+        error: null
+      }
+    }
+
+    divide (count : Count) {
+      return {
+        success: this.result,
+        error: null
+      }
+    }
+
+    result (count : Count) {
+      return {
+        success: this.result,
+        error: null
+      }
+    }
+  }
+  return new CalculatorStub()
+}
+
+describe('Calculator', () => {
+  let calculator: Calculator;
+
+  test('Successfully add two numbers', () => {
+    const repositoryStub = makeRepositoryStub()
+    const calculatorInstance = new Calculator(repositoryStub)
+    expect(calculatorInstance.add({ num_a: 5, num_b: 3 })).toEqual(8);
+  });
+
+  test('Successfully subtract two numbers', () => {
+    const repositoryStub = makeRepositoryStub()
+    const calculatorInstance = new Calculator(repositoryStub)
+    expect(calculatorInstance.subtract({ num_a: 10, num_b: 3 })).toEqual(7);
+  });
+
+  test('Successfully multiply two numbers', () => {
+    const repositoryStub = makeRepositoryStub()
+    const calculatorInstance = new Calculator(repositoryStub)
+    expect(calculatorInstance.multiply({ num_a: 2, num_b: 3 })).toEqual(6);
+  });
+
+  test('Successfully divide two numbers', () => {
+    const repositoryStub = makeRepositoryStub()
+    const calculatorInstance = new Calculator(repositoryStub)
+    expect(calculatorInstance.divide({ num_a: 20, num_b: 5 })).toEqual(4);
+  });
+
+  test('Divide by zero', () => {
+    const repositoryStub = makeRepositoryStub();
+    const calculatorInstance = new Calculator(repositoryStub);
+    expect(() => calculatorInstance.divide({ num_a: 10, num_b: 0 })).toThrowError("Division by zero is not allowed");
+  });
+  
+});
