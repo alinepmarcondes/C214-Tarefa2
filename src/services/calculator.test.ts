ad616975483c9a9d17dc2cd58505a32cf3562952
alinepmarcondes <aline.patta@ges.inatel.br>
2024-05-03 11:35:59 -0300

    fix: add constructor

--- src/services/calculator.test.ts
+++ src/services/calculator.test.ts
@@ -4,6 +4,11 @@ import { Count } from "../models/Calc";
 
 const makeRepositoryStub = (): CalculatorRepository => {
   class CalculatorStub implements CalculatorRepository {
+    result: number
+    constructor (){
+      this.result = 0
+    }
+
     add (count : Count) {
       return {
         success: true,
@@ -31,13 +36,6 @@ const makeRepositoryStub = (): CalculatorRepository => {
         error: null
       }
     }
-
-    result (count : Count) {
-      return {
-        success: this.result,
-        error: null
-      }
-    }
   }
   return new CalculatorStub()
 }
@@ -46,26 +44,26 @@ describe('Calculator', () => {
   let calculator: Calculator;
 
   test('Successfully add two numbers', () => {
-    const repositoryStub = makeRepositoryStub()
-    const calculatorInstance = new Calculator(repositoryStub)
+    const repository = makeRepositoryStub()
+    const calculatorInstance = new Calculator(repository)
     expect(calculatorInstance.add({ num_a: 5, num_b: 3 })).toEqual(8);
   });
 
   test('Successfully subtract two numbers', () => {
-    const repositoryStub = makeRepositoryStub()
-    const calculatorInstance = new Calculator(repositoryStub)
+    const repository = makeRepositoryStub()
+    const calculatorInstance = new Calculator(repository)
     expect(calculatorInstance.subtract({ num_a: 10, num_b: 3 })).toEqual(7);
   });
 
   test('Successfully multiply two numbers', () => {
-    const repositoryStub = makeRepositoryStub()
-    const calculatorInstance = new Calculator(repositoryStub)
+    const repository = makeRepositoryStub()
+    const calculatorInstance = new Calculator(repository)
     expect(calculatorInstance.multiply({ num_a: 2, num_b: 3 })).toEqual(6);
   });
 
   test('Successfully divide two numbers', () => {
-    const repositoryStub = makeRepositoryStub()
-    const calculatorInstance = new Calculator(repositoryStub)
+    const repository = makeRepositoryStub()
+    const calculatorInstance = new Calculator(repository)
     expect(calculatorInstance.divide({ num_a: 20, num_b: 5 })).toEqual(4);
   });
   
